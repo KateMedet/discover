5544e786bbcccd26a1ef279be7e95a24f43f23c1
KateMedet <kniklaso@ucsd.edu>
2017-01-15 16:30:14 -0800

    Update environment.py

--- notebooks/environment.py
+++ notebooks/environment.py
@@ -1,24 +1,34 @@
-
-######### Import libraries #########
-import os
+######### Set up Notebook Project (NB Project) #########
+print('Launching NB Project ...')
 import sys
+from os.path import join, realpath
 
+DIR_PROJECT = realpath('..')
+print('\tExported variable DIR_PROJECT ({}).'.format(DIR_PROJECT))
+
+DIR_TOOLS = join(DIR_PROJECT, 'tools/')
+sys.path.insert(0, DIR_TOOLS)
+print('\tExported variable DIR_TOOLS ({}), and added it to the path.'.format(DIR_TOOLS))
+
+DIR_DATA = join(DIR_PROJECT, 'data/')
+print('\tExported variable DIR_DATA ({}).'.format(DIR_DATA))
+
+DIR_RESULT = join(DIR_PROJECT, 'results/')
+print('\tExported variable DIR_RESULT ({}).'.format(DIR_RESULT))
+
+DIR_MEDIA = join(DIR_PROJECT, 'media/')
+print('\tExported variable DIR_MEDIA ({}).'.format(DIR_MEDIA))
+
+######### Set up project specific parameters #########
+import numpy as np
+import pandas as pd
 import matplotlib as mpl
-
+from statsmodels.sandbox.stats.multicomp import multipletests
+from matplotlib.backends.backend_pdf import PdfPages
 mpl.rcParams['figure.figsize'] = (8, 5)
 mpl.rcParams['figure.max_open_warning'] = 100
-
-######### Set up Simpli NB Project directories #########
-DIR_PROJECT = os.path.join('/Users/kateme/discover')
-DIR_CODE = os.path.join(DIR_PROJECT, 'code')
-DIR_DATA = os.path.join(DIR_PROJECT, 'data')
-DIR_RESULT = os.path.join(DIR_PROJECT, 'results')
-DIR_MEDIA = os.path.join(DIR_PROJECT, 'media')
-
-######### Set up tools #########
-sys.path.insert(0, '../code')
+sys.path.insert(0, DIR_TOOLS)
 import ccal
-
 
 ### Load expressional signatures ###
 ccle_signatures = ccal.read_gct('/Users/kateme/discover/data/CCLE_Expression_Entrez_2012-04-06_plus_Achilles.SELECTED_TISSUES_PATHWAYS.gct')
