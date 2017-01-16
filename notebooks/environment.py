428a669cc17b59751763155189feae4e75ec9c0a
Kate Medet <kate.medetgul.ernar@gmail.com>
2017-01-15 16:59:50 -0800

    cleaned notebook and updated environment.py

--- notebooks/environment.py
+++ notebooks/environment.py
@@ -29,14 +29,3 @@ mpl.rcParams['figure.figsize'] = (8, 5)
 mpl.rcParams['figure.max_open_warning'] = 100
 sys.path.insert(0, DIR_TOOLS)
 import ccal
-
-### Load expressional signatures ###
-ccle_signatures = ccal.read_gct('/Users/kateme/discover/data/CCLE_Expression_Entrez_2012-04-06_plus_Achilles.SELECTED_TISSUES_PATHWAYS.gct')
-ccle_hNSC_G3_150_UP = ccle_signatures.ix['hNSC_G3_150_UP']
-sanger_signatures = ccal.read_gct('/Users/kateme/discover/data/Sanger_Expression.out.SELECTED_TISSUES_PATHWAYS.gct')
-sanger_hNSC_G3_150_UP = sanger_signatures.ix['hNSC_G3_150_UP']
-
-### Load drug sensitivities ###
-ccle_drug_sensitivity = ccal.read_gct('/Users/kateme/discover/data/CCLE_Drug_Sensitivity.gct')
-ctrp_drug_sensitivity = ccal.read_gct('/Users/kateme/discover/data/CTRPv2.2_2015_pub.gct')
-sanger_drug_sensitivity = ccal.read_gct('/Users/kateme/discover/data/drug_sensitivity.out.SELECTED.gct')
